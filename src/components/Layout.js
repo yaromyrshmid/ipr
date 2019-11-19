6f4dd1dde7264a12633b1de93f5d31b8b163f96a
yaromyrshmid <yaromyr.shmid@gmail.com>
2019-11-19 21:26:17 +0200

    layout fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../css/layout.css"
 
 const Layout = ({ children }) => {
-  if (document) {
-    document.cookie = "same-site-cookie=foo; SameSite=Lax"
-    document.cookie = "cross-site-cookie=bar; SameSite=None; Secure"
-  }
   return (
     <main>
       <Navbar />
